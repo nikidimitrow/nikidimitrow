67e4466e3cab454fb28f07ecd6797b7825885bee
Nikolay Dimitrow <nikidimitrow@users.noreply.github.com>
2020-04-03 23:04:29 +0300

    Update P0002-happy_number.cpp

--- solution/01-week/P0002-happy_number.cpp
+++ solution/01-week/P0002-happy_number.cpp
@@ -1,1 +1,8 @@
-/* Link to the problem: https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3284/ /*
+/* Link to the problem: https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3284 */
+
+class Solution {
+public:
+    bool isHappy(int n) {
+        
+    }
+};
